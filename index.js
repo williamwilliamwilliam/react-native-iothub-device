2bccd9ebfdb3ba3d5bd0e6e0290143739181ad3a
William Gammon <wdgammon@gmail.com>
2019-02-02 14:33:27 -0600

    no message

--- index.js
+++ index.js
@@ -2,29 +2,11 @@
 
 import {DeviceEventEmitter, NativeModules} from 'react-native';
 
-export default NativeModules.IoTHubDeviceModule;
-const reportedProperties = {};
 export function connectToHub(connectionString, desiredPropertySubscriptions, connectionSuccess, connectionFailure, onDeviceTwinPropertyRetrieved, onDesiredPropertyUpdate){
-
     DeviceEventEmitter.addListener('onDesiredPropertyUpdate', (event) => {
         if(event.propertyJson){
             const property = JSON.parse(event.propertyJson);
             onDesiredPropertyUpdate(property.property);
-        }
-    });
-
-    /**
-     * On Android, device twin properties are retrieved one-by-one, so we
-     * populate a full map in here to keep a full snapshot of the Device Twin
-     * according to Azure
-     */
-    DeviceEventEmitter.addListener('onDeviceTwinPropertyRetrieved', (event) => {
-        console.log('onDeviceTwinPropertyRetrieved');
-        console.log(event);
-        if(event.propertyJson){
-            const property = JSON.parse(event.propertyJson);
-            reportedProperties[property.property.key] = property;
-            onDeviceTwinPropertyRetrieved(property.property);
         }
     });
 
@@ -34,7 +16,7 @@ export function subscribeToTwinDesiredProperties(propertyKey, success, failure){
     NativeModules.IoTHubDeviceModule.subscribeToTwinDesiredProperties(propertyKey, success, failure);
 }
 
-/**onDeviceTwinPropertyRetrieved
+/** onDeviceTwinPropertyRetrieved
  * {testValue:12345, testValue2:"12345", testValue3: true}
  * @param properties
  */
