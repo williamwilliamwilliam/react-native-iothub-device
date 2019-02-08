d967d0fc41b92226fd02ded2ea9ed5f92de70d5a
William <williamwilliamwilliam@users.noreply.github.com>
2019-02-07 18:57:39 -0600

    Update IoTHubDeviceModule.java

--- android/src/main/java/com/williamwilliamwilliam/iothub/IoTHubDeviceModule.java
+++ android/src/main/java/com/williamwilliamwilliam/iothub/IoTHubDeviceModule.java
@@ -1,8 +1,10 @@
 package com.williamwilliamwilliam.iothub;
 
+import android.os.SystemClock;
 import android.util.Log;
 
 import com.facebook.common.internal.Sets;
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -12,12 +14,27 @@ import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.ReadableType;
+import com.facebook.react.bridge.WritableMap;
 import com.google.gson.Gson;
 import com.microsoft.azure.sdk.iot.device.DeviceClient;
 import com.microsoft.azure.sdk.iot.device.DeviceTwin.Pair;
 import com.microsoft.azure.sdk.iot.device.DeviceTwin.Property;
 import com.microsoft.azure.sdk.iot.device.DeviceTwin.TwinPropertyCallBack;
 import com.microsoft.azure.sdk.iot.device.IotHubClientProtocol;
+import com.microsoft.azure.sdk.iot.device.IotHubConnectionStatusChangeCallback;
+import com.microsoft.azure.sdk.iot.device.IotHubConnectionStatusChangeReason;
+import com.microsoft.azure.sdk.iot.device.IotHubEventCallback;
+import com.microsoft.azure.sdk.iot.device.IotHubMessageResult;
+import com.microsoft.azure.sdk.iot.device.IotHubStatusCode;
+import com.microsoft.azure.sdk.iot.device.Message;
+import com.microsoft.azure.sdk.iot.device.MessageCallback;
+import com.microsoft.azure.sdk.iot.device.exceptions.DeviceClientException;
+import com.microsoft.azure.sdk.iot.device.exceptions.TransportException;
+import com.microsoft.azure.sdk.iot.device.transport.IotHubConnectionStatus;
+import com.microsoft.azure.sdk.iot.device.transport.RetryPolicy;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -25,12 +42,11 @@ import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class IoTHubDeviceModule extends ReactContextBaseJavaModule  {
 
     private TwinPropertyCallBack onDesiredPropertyUpdate;
-    private OnDeviceTwinPropertyRetrieved onDeviceTwinPropertyRetrieved;
-    private OnDeviceTwinStatus onDeviceTwinStatus;
 
     private Gson gson = new Gson();
 
@@ -40,10 +56,6 @@ public class IoTHubDeviceModule extends ReactContextBaseJavaModule  {
     }
 
     public EmitHelper emitHelper = new EmitHelper();
-    public IoTHubMessageCallback messageCallback;
-
-    public final static String EVENT_NAME_MESSAGE_RECEIVED = "onMessageReceived";
-
     public ReactContext getReactContext(){
         return getReactApplicationContext();
     }
@@ -51,18 +63,7 @@ public class IoTHubDeviceModule extends ReactContextBaseJavaModule  {
     public IoTHubDeviceModule(ReactApplicationContext reactContext) {
         super(reactContext);
         reactContext.addLifecycleEventListener(new IoTHubLifecycleEventListener());
-        messageCallback = new IoTHubMessageCallback(reactContext);
-        onDesiredPropertyUpdate = new OnDesiredPropertyUpdate(this, getReactApplicationContext());
-        onDeviceTwinPropertyRetrieved = new OnDeviceTwinPropertyRetrieved(getReactApplicationContext());
-        onDeviceTwinStatus = new OnDeviceTwinStatus(getReactApplicationContext());
-    }
-
-    Map<Property, Pair<TwinPropertyCallBack, Object>> desiredPropertySubscriptions = new HashMap<Property, Pair<TwinPropertyCallBack, Object>>();
-
-    Map<String, Property> twinProperties = new HashMap<>();
-
-    public void setTwinProperty(Property property){
-        twinProperties.put(property.getKey(), property);
+        onDesiredPropertyUpdate = new OnDesiredPropertyUpdate(getReactApplicationContext());
     }
 
     @ReactMethod
@@ -115,35 +116,99 @@ public class IoTHubDeviceModule extends ReactContextBaseJavaModule  {
         }
     }
 
-    @ReactMethod
-    public void subscribeToTwinDesiredProperties(String desiredProperty, Promise promise) {
-        Log.i(this.getClass().getSimpleName(), "Subscribing to desired property "+desiredProperty);
-        try{
-            desiredPropertySubscriptions.put(new Property(desiredProperty, null), new Pair<TwinPropertyCallBack, Object>(onDesiredPropertyUpdate, null));
-            client.subscribeToTwinDesiredProperties(desiredPropertySubscriptions);
-            promise.resolve(true);
-        }catch(Exception e){
-            Log.e(this.getClass().getSimpleName(), "There was a problem subscribing to a IoT Device Twin property. "+e.getMessage(), e);
-            promise.reject(e);
-        }
-
+    private IotHubEventCallback onDeviceTwinStatusCallback(){
+        return new IotHubEventCallback() {
+            @Override
+            public void execute(IotHubStatusCode responseStatus, Object callbackContext) {
+                Log.d(this.getClass().getSimpleName(), "onDeviceTwinStatusCallback: "+responseStatus);
+                WritableMap params = Arguments.createMap();
+                params.putString("responseStatus", responseStatus.name());
+                emitHelper.emit(getReactContext(), "onDeviceTwinStatusCallback", params);
+            }
+        };
+    }
+    private TwinPropertyCallBack onDeviceTwinPropertyRetrieved(){
+        return new TwinPropertyCallBack() {
+            @Override
+            public void TwinPropertyCallBack(Property property, Object context) {
+                Log.d(this.getClass().getSimpleName(), gson.toJson(property));
+                WritableMap params = Arguments.createMap();
+                params.putString("propertyJson", gson.toJson(property));
+                emitHelper.emit(getReactContext(), "onDeviceTwinPropertyRetrieved", params);
+            }
+        };
     }
 
+    DeviceClient client;
+    IotHubEventCallback onDeviceTwinStatusCallback = onDeviceTwinStatusCallback();
+    TwinPropertyCallBack twinPropertyCallBack = onDeviceTwinPropertyRetrieved();
+    MessageCallback onMessageCallback = onMessageCallback();
+    boolean initialized = false;
+    long retryAfter = 1000;
+    long retryMultiplier = 2;
     @ReactMethod
     public void connectToHub(String connectionString, ReadableArray desiredPropertySubscriptions, Promise promise) {
         try {
-            initializeDeviceClient(connectionString);
-            client.startDeviceTwin(onDeviceTwinStatus,
+
+            client = new DeviceClient(connectionString, IotHubClientProtocol.AMQPS_WS);
+            setConnectionStatusChangeCallback();
+            boolean isConnectionOpened = false;
+            while(!isConnectionOpened){
+                try{
+                    client.open();
+                    isConnectionOpened = true;
+                }catch(Exception e){
+                    if(StringUtils.containsIgnoreCase(ExceptionUtils.getRootCauseMessage(e), "TransportException: Timed out waiting to connect to service")){
+                        Log.w(this.getClass().getSimpleName(), ExceptionUtils.getRootCauseMessage(e)+". Reconnecting in "+(retryAfter/1000)+" seconds.");
+                        SystemClock.sleep(retryAfter);
+                        retryAfter = retryAfter* retryMultiplier;
+                    }else{
+                        throw e;
+                    }
+                }
+            }
+
+            client.startDeviceTwin(onDeviceTwinStatusCallback,
                     null,
-                    onDeviceTwinPropertyRetrieved,
+                    twinPropertyCallBack,
                     null);
+            client.setMessageCallback(onMessageCallback, null);
             subscribeToDesiredProperties(desiredPropertySubscriptions);
+            initialized = true;
             promise.resolve("Successfully connected!");
         } catch (Exception e){
             String message = "There was a problem connecting to IoT Hub. "+e.getMessage();
             Log.e(this.getClass().getSimpleName(), message, e);
             promise.reject(this.getClass().getSimpleName(), e);
         }
+    }
+
+    private MessageCallback onMessageCallback() {
+        return new MessageCallback() {
+            @Override
+            public IotHubMessageResult execute(Message message, Object callbackContext) {
+                String messageString = new String(message.getBytes(), Message.DEFAULT_IOTHUB_MESSAGE_CHARSET);
+                Log.d(this.getClass().getSimpleName(), messageString);
+                WritableMap params = Arguments.createMap();
+                params.putString("message", messageString);
+                params.putString("messageId", message.getMessageId());
+                emitHelper.emit(getReactContext(), "onMessageReceived", params);
+                return IotHubMessageResult.COMPLETE;
+            }
+        };
+    }
+
+    private void setConnectionStatusChangeCallback() {
+        client.registerConnectionStatusChangeCallback(new IotHubConnectionStatusChangeCallback() {
+            @Override
+            public void execute(IotHubConnectionStatus status, IotHubConnectionStatusChangeReason statusChangeReason, Throwable throwable, Object callbackContext) {
+                Log.d(this.getClass().getSimpleName(), "status: "+status+" reason: "+statusChangeReason);
+                WritableMap params = Arguments.createMap();
+                params.putString("status", status.name());
+                params.putString("statusChangeReason", statusChangeReason.name());
+                emitHelper.emit(getReactContext(), "onConnectionStatusChange", params);
+            }
+        }, null);
     }
 
     private void subscribeToDesiredProperties(ReadableArray desiredPropertySubscriptions) throws IOException {
@@ -159,42 +224,10 @@ public class IoTHubDeviceModule extends ReactContextBaseJavaModule  {
         }
     }
 
-    DeviceClient client;
-    private void initializeDeviceClient(String connectionString) throws URISyntaxException, IOException {
-        if(client != null){
-            Log.w(this.getClass().getSimpleName(), "Trying to connect to IoT Hub, but the Device Client already exists. Close and reopen.");
-            client.closeNow();
-        }
-        client = new DeviceClient(connectionString, IotHubClientProtocol.AMQPS_WS);
-        client.open();
-    }
-
 
     @Override
     public Map<String, Object> getConstants() {
         final Map<String, Object> constants = new HashMap<>();
-        constants.put("EVENT_NAME_MESSAGE_RECEIVED", EVENT_NAME_MESSAGE_RECEIVED);
         return constants;
-    }
-    static class Counter
-    {
-        int num;
-
-        Counter(int num) {
-            this.num = num;
-        }
-
-        int get() {
-            return this.num;
-        }
-
-        void increment() {
-            this.num++;
-        }
-
-        @Override
-        public String toString() {
-            return Integer.toString(this.num);
-        }
     }
 }
